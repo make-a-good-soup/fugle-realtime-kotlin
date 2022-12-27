b0176827c71c5f7fe160febb685b4b0bef5ae612
nick chen <nick45chen@gmail.com>
2022-12-27 20:17:59 +0800

    add: todo

--- fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Intraday.kt
+++ fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Intraday.kt
@@ -3,3 +3,5 @@ package net.makeagoodsoup.fugle_realtime_lib.core.entities
 data class Intraday<T>(val apiVersion: String, val data: T)
 
 data class MetaData(val info: Info, val meta: Meta)
+
+// todo: 其他 data model
--- fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Meta.kt
+++ fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Meta.kt
@@ -1,5 +1,6 @@
 package net.makeagoodsoup.fugle_realtime_lib.core.entities
 
+// todo: 完成 data model
 data class Meta(
     val market: String,
     val nameZhTw: String
