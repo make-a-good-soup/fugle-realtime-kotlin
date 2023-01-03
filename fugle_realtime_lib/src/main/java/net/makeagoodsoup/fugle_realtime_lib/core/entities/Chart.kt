253c7b48ac656e8aa8ae5acda4555ab4812cb7f7
Dan Hung <dan12411@gmail.com>
2023-01-03 18:51:57 +0800

    fix: rollback to double.

--- fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Chart.kt
+++ fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/entities/Chart.kt
@@ -1,11 +1,11 @@
 package net.makeagoodsoup.fugle_realtime_lib.core.entities
 
 data class Chart(
-    val averagePrices: List<Number>?, // 當日個股於此分鐘的成交均價
-    val openingPrices: List<Number>?, // 此分鐘的開盤價
-    val highestPrices: List<Number>?, // 此分鐘的最高價
-    val lowestPrices: List<Number>?, // 此分鐘的最低價
-    val closingPrices: List<Number>?, // 此分鐘的收盤價
-    val volumes: List<Number>?, // 此分鐘的成交量 (指數：金額；個股：張數；興櫃股票及零股：股數)
-    val times: List<Number>?, // Unix timestamp (每分鐘單位)
+    val averagePrices: List<Double>?, // 當日個股於此分鐘的成交均價
+    val openingPrices: List<Double>?, // 此分鐘的開盤價
+    val highestPrices: List<Double>?, // 此分鐘的最高價
+    val lowestPrices: List<Double>?, // 此分鐘的最低價
+    val closingPrices: List<Double>?, // 此分鐘的收盤價
+    val volumes: List<Double>?, // 此分鐘的成交量 (指數：金額；個股：張數；興櫃股票及零股：股數)
+    val times: List<Double>?, // Unix timestamp (每分鐘單位)
 )
