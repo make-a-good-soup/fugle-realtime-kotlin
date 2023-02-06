601e10008ddb92466c09521cca1c2d29f0c79eba
nick.chen <nick.chen@fugle.tw>
2023-02-06 23:25:45 +0800

    changed: update sample app ui.

--- app/src/main/java/com/example/fugle_realtime_kotlin_sample/MainActivity.kt
+++ app/src/main/java/com/example/fugle_realtime_kotlin_sample/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.fugle_realtime_kotlin_sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fugle_realtime_kotlin_sample.ui.theme.FuglerealtimekotlinsampleTheme
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -21,33 +27,62 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FuglerealtimekotlinsampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-                Button(onClick = {
-                    GlobalScope.launch {
-                        val result = FugleHttpRepository().getMeta("2884", "demo")
-                        println("get meta: ${result.successOr(null)}")
-                    }
-                }) {
-                    Text(text = "測試")
-                }
+            Body()
+        }
+    }
+}
+
+val resultText = mutableStateOf("")
+
+@Composable()
+fun Body() {
+    return FuglerealtimekotlinsampleTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
+            ) {
+                SendHttpRequestButton()
+                Box(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .fillMaxWidth()
+                )
+                TerminalText()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SendHttpRequestButton() {
+    return Button(onClick = {
+        resultText.value = "InProgress"
+        GlobalScope.launch {
+            val result = FugleHttpRepository().getMeta("2884", "demo")
+            println("get meta: ${result.successOr(null)}")
+            resultText.value = "${result.successOr(null)}"
+        }
+    }) {
+        Text(text = "Get Meta")
+    }
+}
+
+@Composable
+fun TerminalText() {
+    val text by resultText
+    return Text(
+        text = text, color = Color.White, fontSize = 16.sp, modifier = Modifier
+            .background(color = Color.Black)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    FuglerealtimekotlinsampleTheme {
-        Greeting("Android")
-    }
+    Body()
 }
