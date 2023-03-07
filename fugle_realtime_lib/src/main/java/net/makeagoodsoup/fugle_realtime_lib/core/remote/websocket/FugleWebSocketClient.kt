6bdb43c19c83041f73e786f5d76c604dd62c65f6
TeChengHung <dan12411@gmail.com>
2023-03-07 20:54:25 +0800

    feat: add `connectMeta()`. (#18)

--- fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/remote/websocket/FugleWebSocketClient.kt
+++ fugle_realtime_lib/src/main/java/net/makeagoodsoup/fugle_realtime_lib/core/remote/websocket/FugleWebSocketClient.kt
@@ -2,9 +2,11 @@ package net.makeagoodsoup.fugle_realtime_lib.core.remote.websocket
 
 import kotlinx.coroutines.flow.Flow
 import net.makeagoodsoup.fugle_realtime_lib.core.entities.ChartData
+import net.makeagoodsoup.fugle_realtime_lib.core.entities.MetaData
 import net.makeagoodsoup.fugle_realtime_lib.core.entities.QuoteData
 
 interface FugleWebSocketClient {
     fun connectQuote(symbolId: String, apiToken: String): Flow<QuoteData>
     fun connectChart(symbolId: String, apiToken: String): Flow<ChartData>
+    fun connectMeta(symbolId: String, apiToken: String): Flow<MetaData>
 }
