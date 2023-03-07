39ccc51ec45be447f2c6b8eb31c998831d9cd755
nick.chen <nick.chen@fugle.tw>
2023-03-07 20:41:48 +0800

    change: rename FugleRealtimeKotlinSampleTheme to follow jetpack compose naming convention.

--- app/src/main/java/com/example/fugle_realtime_kotlin_sample/MainActivity.kt
+++ app/src/main/java/com/example/fugle_realtime_kotlin_sample/MainActivity.kt
@@ -5,14 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.example.fugle_realtime_kotlin_sample.ui.MainScreen
-import com.example.fugle_realtime_kotlin_sample.ui.theme.FuglerealtimekotlinsampleTheme
+import com.example.fugle_realtime_kotlin_sample.ui.theme.FugleRealtimeKotlinSampleTheme
 
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FuglerealtimekotlinsampleTheme {
+            FugleRealtimeKotlinSampleTheme {
                 MainScreen()
             }
         }
